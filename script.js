8643d6a8e88dee656b83fd02e6e8d69e25dee131
Patrick Yoon <yoon@Patricks-MacBook-Air.local>
2023-02-14 02:33:48 -0500

    cleaned up code and allowed hover for new circles

--- script.js
+++ script.js
@@ -1,125 +1,46 @@
+// data points
+let dataset1 = [
+  [1, 2],
+  [2, 4],
+  [6, 2],
+  [9, 9],
+];
+
 // onmouseover and onmouseout functions
-function mouseOverCircle1() {
-  let circle1 = document.getElementById("circle1");
-  circle1.style.fill = "blue";
+function mouseOverCircle(hovered_id) {
+  let circle = document.getElementById(hovered_id);
+  circle.style.fill = "blue";
+  
 }
 
-function mouseOutCircle1() {
-  let circle1 = document.getElementById("circle1");
-  circle1.style.fill = "pink";
+function mouseOutCircle(out_id) {
+  let circle = document.getElementById(out_id);
+  circle.style.fill = "pink";
+  
 }
 
-function mouseOverCircle2() {
-  let circle2 = document.getElementById("circle2");
-  circle2.style.fill = "blue";
-}
-
-function mouseOutCircle2() {
-  let circle2 = document.getElementById("circle2");
-  circle2.style.fill = "pink";
-}
-
-function mouseOverCircle3() {
-  let circle3 = document.getElementById("circle3");
-  circle3.style.fill = "blue";
-}
-
-function mouseOutCircle3() {
-  let circle3 = document.getElementById("circle3");
-  circle3.style.fill = "pink";
-}
-
-function mouseOverCircle4() {
-  let circle4 = document.getElementById("circle4");
-  circle4.style.fill = "blue";
-}
-
-function mouseOutCircle4() {
-  let circle4 = document.getElementById("circle4");
-  circle4.style.fill = "pink";
-}
-
-function mouseOverNewCircle() {
-  let newCircle = document.getElementById("newCircle");
-  newCircle.style.fill = "blue";
-  console.log("here");
-}
-
-function mouseOutNewCircle() {
-  let newCircle = document.getElementById("newCircle");
-  newCircle.style.fill = "pink";
-}
-
-// onclick functions
-let isCircle1Clicked = false;
-let isCircle2Clicked = false;
-let isCircle3Clicked = false;
-let isCircle4Clicked = false;
-
-let clickedArr = [];
-
-function onClickCircle1() {
-  let circle1 = document.getElementById("circle1");
+function onClickCircle(clicked_id) {
+  let circle = document.getElementById(clicked_id);
   let pointMsg = document.getElementById("lastPointClicked").innerHTML;
-  let coord = "(1, 2)\n\n";
 
-  if (circle1.style.stroke === "green") {
-    circle1.style.stroke = "pink";
-    circle1.style.strokeWidth = 0;
+  cx = circle.cx.baseVal.value;
+  x = (cx / 200) * 10;
+
+  cy = circle.cy.baseVal.value;
+  y = 10 - ((cy / 200) * 10);
+
+  let coord = "(" + x + ", " + y + ")\n\n";
+
+  if (circle.style.stroke === "green") {
+    circle.style.stroke = "pink";
+    circle.style.strokeWidth = 0;
     document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
   } else {
-    circle1.style.stroke = "green";
-    circle1.style.strokeWidth = 3;
+    circle.style.stroke = "green";
+    circle.style.strokeWidth = 3;
     document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
   }
-}
-
-function onClickCircle2() {
-  let circle2 = document.getElementById("circle2");
-  let pointMsg = document.getElementById("lastPointClicked").innerHTML;
-  let coord = "(2, 4)\n\n";
-
-  if (circle2.style.stroke === "green") {
-    circle2.style.stroke = "pink";
-    circle2.style.strokeWidth = 0;
-    document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
-  } else {
-    circle2.style.stroke = "green";
-    circle2.style.strokeWidth = 3;
-    document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
-  }
-}
-
-function onClickCircle3() {
-  let circle3 = document.getElementById("circle3");
-  let pointMsg = document.getElementById("lastPointClicked").innerHTML;
-  let coord = "(6, 2)\n\n";
-
-  if (circle3.style.stroke === "green") {
-    circle3.style.stroke = "pink";
-    circle3.style.strokeWidth = 0;
-    document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
-  } else {
-    circle3.style.stroke = "green";
-    circle3.style.strokeWidth = 3;
-    document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
-  }
-}
-
-function onClickCircle4() {
-  let circle4 = document.getElementById("circle4");
-  let pointMsg = document.getElementById("lastPointClicked").innerHTML;
-  let coord = "(9, 9)\n\n";
-
-  if (circle4.style.stroke === "green") {
-    circle4.style.stroke = "pink";
-    circle4.style.strokeWidth = 0;
-    document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
-  } else {
-    circle4.style.stroke = "green";
-    circle4.style.strokeWidth = 3;
-    document.getElementById("lastPointClicked").innerHTML = pointMsg + coord;
-  }
+  
 }
 
 // finding the index of values in a given array
@@ -132,13 +53,7 @@ function findIndexInArray(givenArray, object) {
   return -1;
 }
 
-// data points
-let dataset1 = [
-  [1, 2],
-  [2, 4],
-  [6, 2],
-  [9, 9],
-];
+let circleIDCounter = 5;
 
 // adding in new coordinates
 function addPoint() {
@@ -161,13 +76,28 @@ function addPoint() {
       "http://www.w3.org/2000/svg",
       "circle"
     );
-    newCircle.setAttribute("id", "newCircle");
+
+    let circleID = "circle" + circleIDCounter;
+    circleIDCounter++;
+
+    newCircle.setAttribute("id", circleID);
     newCircle.setAttribute("cx", (x / 10) * 200);
     newCircle.setAttribute("cy", ((10 - y) / 10) * 200);
     newCircle.setAttribute("r", 10);
     newCircle.style.fill = "pink";
-    // newCircle.addEventListener("onmouseover", mouseOverNewCircle());
-    // newCircle.addEventListener("onmouseout", mouseOutNewCircle());
+
+    newCircle.onmouseover = function() {
+      mouseOverCircle(circleID);
+    };
+
+    newCircle.onmouseout =  function() {
+      mouseOutCircle(circleID);
+    };
+
+    newCircle.onclick = function() {
+      onClickCircle(circleID);
+    };
+
     document.querySelector("svg").appendChild(newCircle);
     console.log("appended");
   }
